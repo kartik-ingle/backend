b31d6ce9bd043835cd45e5c3421a2cfeb6ee8b8c
kartik Ingle <ikartik1211@gmail.com>
2025-07-31 16:28:56 +0530

    all setup configured

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -5,7 +5,7 @@ const storage = multer.diskStorage({
         cb(null, "./public/temp");
     },
     filename: function (req, file, cb) {
-        cb(null, FileSystemFileEntry.originalname)
+        cb(null, file.originalname)
     }
 })
 
